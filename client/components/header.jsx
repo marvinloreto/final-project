69e4a3acc77de714c8dc5706c65f1e1d6b320a9d
Marvin Loreto <marvinjloreto@gmail.com>
2023-02-28 23:04:59 +0000

    Completed Menu Modal component: issue-2

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
 
-export default function Header(props) {
-  return (
-    <header>
-      <div className="container">
-        <div className="row header-row">
-          <div className="col-1">
-            <i className="fa-solid fa-bars" />
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  }
+
+  render() {
+    const hamburger = this.state.isOpen ? 'hidden' : 'fa-solid fa-bars menu-bars';
+    const toggleMenu = this.state.isOpen ? 'menu' : 'hidden';
+    const modalBackground = this.state.isOpen ? 'modal' : 'hidden';
+
+    return (
+      <div>
+        <header>
+          <div className='container'>
+            <div className='row header-row'>
+              <div className='col-1'>
+                <i className={hamburger} onClick={this.handleClick} />
+              </div>
+              <div className='col-10'>
+                <h1 className='header'>Gym Tracker</h1>
+              </div>
+            </div>
           </div>
-          <div className="col-10">
-            <h1 className="header">Gym Tracker</h1>
-          </div>
+        </header>
+        <div className={toggleMenu}>
+          <ul className='list'>
+            <li onClick={this.handleClick}><a className='menu-button' href='#'>Home</a></li>
+            <li onClick={this.handleClick}><a className='menu-button' href='#workouts'>Workouts</a></li>
+            <li onClick={this.handleClick}><a className='menu-button' href='#'>Favorites</a></li>
+          </ul>
+          <div className={modalBackground} onClick={this.handleClick} />
         </div>
-      </div>
-    </header>
-  );
+      </div >
+    );
+  }
+
 }
---
+++ client/components/month-selector.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class MonthSelector extends React.Component {
+
+}
