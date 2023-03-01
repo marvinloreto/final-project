b3a6da0c245a05270ca0d94e98bc327b19e95f4d
Marvin Loreto <marvinjloreto@gmail.com>
2023-03-01 17:29:49 +0000

    Fixed menu but no modal overlay

--- client/app.jsx
+++ client/app.jsx
@@ -33,8 +33,8 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <Header />
         <MenuModal />
+        <Header />
         {this.renderPage()}
       </>
     );
--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -3,34 +3,38 @@ import React from 'react';
 export default class MenuModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isOpen: false };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+    this.handleClickModal = this.handleClickModal.bind(this);
+    this.handleClickItem = this.handleClickItem.bind(this);
   }
 
-  handleClick() {
-    const { isOpen } = this.state;
-    this.setState({ isOpen: !isOpen });
+  handleClickModal() {
+    this.setState({ isClicked: true });
+  }
+
+  handleClickItem() {
+    this.setState({ isClicked: false });
   }
 
   render() {
-    const hamburger = this.state.isOpen ? 'hidden' : 'fa-solid fa-bars menu-bars';
-    const toggleMenu = this.state.isOpen ? 'menu' : 'hidden';
-    const modalBackground = this.state.isOpen ? 'modal' : 'hidden';
-
-    return (
-      <div>
-        <div className='button'>
-          <i className={hamburger} onClick={this.handleClick} />
+    if (this.state.isClicked) {
+      return (
+        <div className='overlay' onClick={this.handleClickItem}>
+          <div>
+            <div className='menu'>
+              <ul className='list'>
+                <li onClick={this.handleClickItem}><a className="menu-button" href="#">Home</a></li>
+                <li onClick={this.handleClickItem}><a className="menu-button" href="#workouts">Workouts</a></li>
+                <li onClick={this.handleClickItem}><a className="menu-button" href="#">Favorites</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className={toggleMenu}>
-          <ul className='list'>
-            <li onClick={this.handleClick}><a className='menu-button' href='#'>Home</a></li>
-            <li onClick={this.handleClick}><a className='menu-button' href='#workouts'>Workouts</a></li>
-            <li onClick={this.handleClick}><a className='menu-button' href='#'>Favorites</a></li>
-          </ul>
-          <div className={modalBackground} onClick={this.handleClick} />
-        </div>
-      </div >
-    );
+      );
+    } else if (!this.state.isClicked) {
+      return (
+        <i className="fa-solid fa-bars menu-bars" onClick={this.handleClickModal} />
+      );
+    }
   }
 }
