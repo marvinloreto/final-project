e344ce7ca2aee9ccb7acb9004ce98943f4ca5a01
Marvin Loreto <marvinjloreto@gmail.com>
2023-03-04 00:51:11 +0000

    Double checked workoutList component

--- client/components/workoutList.jsx
+++ client/components/workoutList.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default function WorkoutList(props) {
-  const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
   return (
     <ul>
       {props.workouts.map(array => {
-        const workoutDates = new Date(array.date).toLocaleDateString('en-US', options);
+        const workoutDay = new Date(array.date).getUTCDate();
+        const workoutMonth = Number(new Date(array.date).getUTCMonth()) + 1;
+        const workoutYear = new Date(array.date).getUTCFullYear();
         return (
           <li className='workout-dates' key={array.workoutId}>
-            <button onClick={ e => props.dateSelect(e, array.workoutId)} key={array.workoutId} className='slash-dates'>{workoutDates} <br /> Workouts: 1</button>
+            <button onClick={ e => props.dateSelect(e, array.workoutId)} key={array.workoutId} className='slash-dates'>{workoutMonth} / {workoutDay} / {workoutYear} <br /> Workouts: 1</button>
           </li>
         );
       })}
